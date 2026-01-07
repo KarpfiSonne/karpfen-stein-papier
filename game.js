5087168393935c8bc6ff13b0c3d7c2cfc190d81e
KarpfiSonne <245768272+KarpfiSonne@users.noreply.github.com>
2026-01-07 21:03:29 +0100

    Implement prompt for human choice

--- game.js
+++ game.js
@@ -1,10 +1,20 @@
 function getComputerChoice() {
     let rng = Math.random() * 100 +1;
-    if (rng <= 33) {
+    if (rng < 33.33) {
         return "karpfen"
-    } else if (rng <= 66) {
+    } else if (rng < 66.66) {
         return "stein"
     } else {
         return "papier"
     }
+}
+
+function getHumanChoice() {
+    choice = prompt("Karpfen, Stein oder Papier?").toLowerCase();
+    if (choice == "karpfen" || choice == "stein" || choice == "papier") {
+        console.log(`Du hast ${choice} gewählt`);
+    } else {
+        console.log("was soll das?");
+        getHumanChoice();
+    } return choice;
 }
